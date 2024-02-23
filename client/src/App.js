8919a4e2dc1070806559b95421fd0588d2ba2fb2
danielaebazan <daniela.e.bazan@hotmail.com>
2024-02-23 13:23:43 -0600

    client and server folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,12 @@
+import ListHeader from './components/ListHeader'
+
 function App() {
   return (
-    <div>
+    <div className="app">
+      <ListHeader/>
     </div>
   );
 }
 
 export default App;
+
