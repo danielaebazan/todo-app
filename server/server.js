6ffe861133a41d0afe26c2bb77db1686897ceccd
danielaebazan <daniela.e.bazan@hotmail.com>
2024-02-27 22:33:07 -0600

    edit toDo

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.get('/todos/:userEmail', async (req, res) => {
        const todos = await pool.query('SELECT * FROM todos WHERE user_email = $1', [userEmail]) 
        res.json(todos.rows)
     } catch (err) {
-        console.error(error)
+        console.error(err)
     }
 })
 
@@ -33,5 +33,21 @@ app.post('/todos', async (req, res) => {
         console.error(err)
     }
 })
+
+// edit a new todo
+app.put('/todos/:id', async (req, res) => {
+    const {id} = req.params
+    const {user_email, title, progress, date} = req.body
+    try {
+        const editToDo = await pool.query('UPDATE todos SET user_email = $1, title = $2, progress = $3, date = $4 WHERE id = $5;', [user_email, title, progress, date, id])
+        res.json(editToDo)
+    } catch(err) {
+        console.error(err)
+    }
+})
+
+
+
+
 
 app.listen(PORT, ( )=> console.log(`Server running on PORT ${PORT}`))
