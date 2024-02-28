b3ca2c7b3aae61369d44d53413b476d18c25af33
danielaebazan <daniela.e.bazan@hotmail.com>
2024-02-27 22:09:43 -0600

    new Todo

--- client/src/App.js
+++ client/src/App.js
@@ -25,8 +25,8 @@ const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
 
   return (
     <div className="app">
-      <ListHeader listName={'🏝️ Holiday tick list'}/>
-      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} />)}
+      <ListHeader listName={'🏝️ Holiday tick list'} getData={getData}/>
+      {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
     </div>
   );
 }
