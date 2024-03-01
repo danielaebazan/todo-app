ddf51bf22a49128f7c8931e78f98a0f9f4c19e58
danielaebazan <daniela.e.bazan@hotmail.com>
2024-02-29 20:33:47 -0600

    login and sign up

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import ListHeader from './components/ListHeader'
 import ListItem from './components/ListItem'
+import Auth from './components/Auth'
 import { useEffect, useState } from 'react'
+import { useCookies } from 'react-cookie'
 
 const App = () => {
-  const userEmail = 'daniela@test.com'
+  const [cookies, setCookie, removeCookie] = useCookies(null)
+  const authToken = cookies.AuthToken
+  const userEmail = cookies.Email
   const [ tasks, setTasks] = useState(null)
 
 const getData = async () => {
   try {
-    const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+    const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${userEmail}`)
     const json = await response.json()
     setTasks(json)
   } catch (err) {
@@ -16,7 +20,11 @@ const getData = async () => {
   }
 }
 
-useEffect(() => getData, [])
+useEffect(() => {
+  if (authToken) {
+    getData()
+  }} 
+, [])
 
 console.log(tasks)
 
@@ -25,8 +33,14 @@ const sortedTasks = tasks?.sort((a,b) => new Date(a.date) - new Date(b.date))
 
   return (
     <div className="app">
+      { !authToken && <Auth/> }
+      {authToken && 
+      <>
       <ListHeader listName={'🏝️ Holiday tick list'} getData={getData}/>
+      <p className="user-email">Welcome back {userEmail}</p>
       {sortedTasks?.map((task) => <ListItem key={task.id} task={task} getData={getData} />)}
+      </>}
+      <p className="copyright">Creative Coding LLC</p>
     </div>
   );
 }
--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,12 @@
 import {useState} from 'react'
+import { useCookies } from 'react-cookie'
 
 const Modal = ({mode, setShowModal, getData, task}) => {
+  const [cookies, setCookie, removeCookie] = useCookies(null)
   const editMode = mode === 'edit' ? true : false
 
   const [data, setData] = useState({
-    user_email: editMode ? task.user_email : 'daniela@test.com',
+    user_email: editMode ? task.user_email : cookies.Email,
     title: editMode ? task.title : null,
     progress: editMode ? task.progress : 50,
     date: editMode ? task.date : new Date()
@@ -13,7 +15,7 @@ const Modal = ({mode, setShowModal, getData, task}) => {
   const postData = async (e) => {
     e.preventDefault()
     try {
-      const response = await fetch('http://localhost:8000/todos', {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos`, {
         method: "POST",
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
@@ -33,7 +35,7 @@ const Modal = ({mode, setShowModal, getData, task}) => {
   const editData = async(e) => {
     e.preventDefault()
     try {
-      const response = await fetch(`http://localhost:8000/todos/${task.id}`, {
+      const response = await fetch(`${process.env.REACT_APP_SERVERURL}/todos/${task.id}`, {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
